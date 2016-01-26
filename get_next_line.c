822c0c1cfb4a59a83149554407f65a6bebc928df
Guillaume RICHARD <guiricha@e3r8p11.42.fr>
2016-01-26 16:03:00 +0100

    bleh

--- get_next_line.c
+++ get_next_line.c
@@ -6,33 +6,98 @@
 /*   By: guiricha <guiricha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/25 16:16:56 by guiricha          #+#    #+#             */
-/*   Updated: 2016/01/25 17:02:26 by guiricha         ###   ########.fr       */
+/*   Updated: 2016/01/26 15:58:54 by guiricha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include "libft.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 int	read_buf(int const fd, t_stock *d)
 {
-	int retread;
-
 	ft_bzero(d->buff, BUFF_SIZE + 1);
-	d->inbuf = 0;
-	d->sibuf = read(fd, d->buff, BUFF_SIZE);
-		return (d->sibuf);
+	d->ib = 0;
+	if (fd < 0)
+		return (-1);
+	d->sb = read(fd, d->buff, BUFF_SIZE);
+	return (d->sb);
+}
+
+int	gnl_do(int const fd, char **line, t_stock *d)
+{
+	char	*tmp;
+	int		size;
+	int		i;
+	int		ret;
+
+	tmp = *line;
+	size = 0;
+	ret = 1;
+	while (42)
+	{
+		if (d->ib >= d->sb)
+			ret = read_buf(fd, d);
+		if (ret == -1 || ret == 0)
+			return (ret);
+		i = 0;
+		while (d->buff[i + d->ib] && d->buff[i + d->ib] != '\n' && d->ib + i < d->sb)
+			i++;
+		if (i)
+		{
+			tmp = *line;
+			if (!(*line = (char *)malloc(sizeof(char) * (size + i + 1))))
+				return (-1);
+			if (size)
+				ft_strcpy(*line, tmp);
+			ft_strncpy(*line + size, d->buff + d->ib, (size_t)i);
+			size += i;
+			d->ib += i;
+		}
+		if (!i)
+		{
+			if (d->buff[d->ib] == '\n' && d->ib < d->sb)
+			{
+				d->ib++;
+				return (1);
+			}
+			else
+			{
+				d->ib++;
+				return (0);
+			}
+		}
+	}
+	return (ret);
 }
 
 int	get_next_line(int const fd, char **line)
 {
 	static t_stock	*d;
-	char *bck;
+	int				ret;
 
+	ret = 1;
+	if (!line)
+		return (-1);
+	*line = NULL;
 	if (!d)
 	{
-		if (!(*d = (t_stock*)malloc(sizeof(t_stock))))
+		if (!(d = (t_stock*)malloc(sizeof(t_stock))))
 			return (-1);
-		if (!((*data)->buff = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		if (!((d)->buff = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1))))
 			return (-1);
-		if (!(readbuf(fd, d)))
+		ret = read_buf(fd, d);
+		if (ret == -1 || ret == 0)
+			return (ret);
 	}
-	if ()
-	error_tests();
+	if (d->ib >= d->sb)
+		ret = read_buf(fd, d);
+	if(ret == -1 || ret == 0)
+		return (ret);
+	ret = gnl_do(fd, line, d);
+	return (ret);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: guiricha <guiricha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/25 16:17:00 by guiricha          #+#    #+#             */
-/*   Updated: 2016/01/25 16:47:40 by guiricha         ###   ########.fr       */
+/*   Updated: 2016/01/26 15:56:52 by guiricha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int				get_next_line(int fd, char **line);
 
 typedef struct	s_stock
 {
-	int			inbuf;
-	int			sibuf;
+	int			ib;
+	int			sb;
 	char		*buff;
 }				t_stock;
 
